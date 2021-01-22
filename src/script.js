04325074ed7504825c230dbca7bf40d25dc5c49b
Frakkir <kulikolga@gmail.com>
2021-01-22 17:15:49 -0600

    weather icon change

--- src/script.js
+++ src/script.js
@@ -46,6 +46,10 @@ function showTemperature(response) {
   city.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 function getCurrent() {
