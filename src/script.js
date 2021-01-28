6fe02277e8078b14ff32dd49574d3fe64636e00d
Frakkir <kulikolga@gmail.com>
2021-01-27 18:45:24 -0600

    farenhight and celsius

--- src/script.js
+++ src/script.js
@@ -50,6 +50,8 @@ function showTemperature(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
+  celsiusTemperature = response.data.main.temp;
+
 }
 
 function getCurrent() {
@@ -69,3 +71,33 @@ function showPosition(position) {
 
 let currentButton = document.querySelector("#currentButton");
 currentButton.addEventListener("click", getCurrent);
+
+
+
+function displayFahrenheitTemperature (event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusTemperature = null; 
+ 
+function displayCelsiusTemperature (event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature); 
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
